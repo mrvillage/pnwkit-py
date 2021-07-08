d83d919a8ea92d1031a5704569149aba157a5904
Village05 <mr.village05@gmail.com>
2021-07-08 16:24:51 -0400

    Removed theme from repo

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 # html_theme = "furo"
 html_theme = "pydata_sphinx_theme"
-html_theme_path = ["."]
 html_logo = "_static/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
