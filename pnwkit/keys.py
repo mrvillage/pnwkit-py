730f0a4d14c58c601af85186a21671a32f47112d
Village <mr.village05@gmail.com>
2022-04-14 23:27:23 -0400

    Fix global set_bot_key

--- pnwkit/keys.py
+++ pnwkit/keys.py
@@ -1,5 +1,6 @@
 API_KEY = None
 BOT_KEY = None
+BOT_KEY_API_KEY = None
 
 
 def set_key(api_key: str) -> None:
@@ -18,17 +19,21 @@ def set_key(api_key: str) -> None:
     pnwkit.set_key(api_key)
 
 
-def set_bot_key(bot_key: str) -> None:
+def set_bot_key(bot_key: str, api_key: str) -> None:
     """Sets the default bot key and the bot key on the :data:`pnwkit` and :data:`async_pnwkit` instances.
 
     Parameters
     ----------
     bot_key : str
         A Politics and War Verified Bot Key.
+    api_key : str
+        The API Key corresponding to the API Key.
     """
     from .core import async_pnwkit, pnwkit
 
     global BOT_KEY
+    global BOT_KEY_API_KEY
     BOT_KEY = bot_key
-    async_pnwkit.set_bot_key(bot_key)
-    pnwkit.set_bot_key(bot_key)
+    BOT_KEY_API_KEY = api_key
+    async_pnwkit.set_bot_key(bot_key, api_key)
+    pnwkit.set_bot_key(bot_key, api_key)
